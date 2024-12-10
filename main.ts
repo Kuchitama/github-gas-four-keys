ed78580941ed2ee3b1284cf2f8a571e017bc8349
Kuchitama <kiyo.kunihira@gmail.com>
2024-12-10 18:11:51 +0900

    Extract FourKeysSheet

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { PullRequest } from "./types/main";
-import { SettingsSheet } from "./src/Sheets";
+import { FourKeysSheet, SettingsSheet } from "./src/Sheets";
 
 const githubEndpoint = "https://api.github.com/graphql";
 
@@ -30,144 +30,9 @@ function initialize() {
     ]])
     .setBackgroundRGB(224, 224, 224);
 
-  const settingsSheet = new SettingsSheet();
-  const settingsSheetName = settingsSheet.sheetName;
-  settingsSheet.initialize(pullRequestsSheetName);
-
-  const fourKeysSheetName = "FourKeys計測結果";
-  const fourKeysSheet = getOrCreateSheet(fourKeysSheetName);
-
-  fourKeysSheet.getRange(1, 1, 2, 9)
-    .setValues([
-      [
-        "集計日",
-        "デプロイ頻度", "",
-        "変更のリードタイム", "",
-        "変更失敗率", "",
-        "平均復旧時間", ""
-      ],
-      [
-        "",
-        "回数", "ランク",
-        "時間(hours)", "ランク",
-        "割合(%)", "ランク",
-        "時間(hours)", "ランク"
-      ]])
-    .setBackgroundRGB(
-      224, 224, 224
-    );
-  const formatRanges = [
-    fourKeysSheet.getRange("C3:C1000"),
-    fourKeysSheet.getRange("E3:E1000"),
-    fourKeysSheet.getRange("G3:G1000"),
-    fourKeysSheet.getRange("I3:I1000")
-  ];
-  const rules = fourKeysSheet.getConditionalFormatRules();
-  rules.push(SpreadsheetApp.newConditionalFormatRule()
-    .whenTextEqualTo("Elite")
-    .setBackground("#b7e1cd")
-    .setRanges(formatRanges)
-    .build());
-  rules.push(SpreadsheetApp.newConditionalFormatRule()
-    .whenTextEqualTo("High")
-    .setBackground("#c9daf8")
-    .setRanges(formatRanges)
-    .build());
-  rules.push(SpreadsheetApp.newConditionalFormatRule()
-    .whenTextEqualTo("Low")
-    .setBackground("#f4cccc")
-    .setRanges(formatRanges)
-    .build());
-  rules.push(SpreadsheetApp.newConditionalFormatRule()
-    .whenTextEqualTo("Medium")
-    .setBackground("#fff2cc")
-    .setRanges(formatRanges)
-    .build());
-  fourKeysSheet.setConditionalFormatRules(rules);
-
-  fourKeysSheet.getRange(3, 2, 1, 8)
-    .setValues([[
-      `=SUM(MAP('${settingsSheetName}'!B$2:B$1000, '${settingsSheetName}'!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${settingsSheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!I$2:I$100000000, FALSE, '${pullRequestsSheetName}'!A$2:A$100000000, a), 0))))/'${settingsSheetName}'!E$2`,
-      `=IFS(B3>='${settingsSheetName}'!E$5, "Elite", B3>='${settingsSheetName}'!E$6, "High", B3>='${settingsSheetName}'!E$7, "Medium", TRUE, "Low")`,
-      `=IF(B3 > 0, SUM(MAP('${settingsSheetName}'!B$2:B$1000, '${settingsSheetName}'!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), SUMIFS('${pullRequestsSheetName}'!G$2:G$100000000, '${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${settingsSheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!I$2:I$100000000, FALSE, '${pullRequestsSheetName}'!A$2:A$100000000, a), 0)))) / (B3*'${settingsSheetName}'!E$2), 0)`,
-      `=IFS(D3<='${settingsSheetName}'!E$8, "Elite", D3<='${settingsSheetName}'!E$9, "High", D3<='${settingsSheetName}'!E$10, "Medium", TRUE, "Low")`,
-      `=IF(B3 > 0, SUM(MAP(${settingsSheetName}!B$2:B$1000, ${settingsSheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${settingsSheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!I$2:I$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a), 0))))/(B3*'${settingsSheetName}'!E$2), 0)`,
-      `=IFS(F3<='${settingsSheetName}'!E$11, "Elite", F3<='${settingsSheetName}'!E$12, "High", F3<='${settingsSheetName}'!E$13, "Medium", TRUE, "Low")`,
-      `=IF(SUM(MAP(${settingsSheetName}!B$2:B$1000, ${settingsSheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${settingsSheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!J$2:J$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a))))) > 0, SUM(MAP(${settingsSheetName}!B$2:B$1000, ${settingsSheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), SUMIFS('${pullRequestsSheetName}'!G$2:G$100000000, '${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${settingsSheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!J$2:J$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a)))))/SUM(MAP(${settingsSheetName}!B$2:B$1000, ${settingsSheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${settingsSheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!J$2:J$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a))))), 0)`,
-      `=IFS(H3<='${settingsSheetName}'!E$14, "Elite", H3<='${settingsSheetName}'!E$15, "High", H3<='${settingsSheetName}'!E$16, "Medium", TRUE, "Low")`
-    ]])
-    .setNumberFormats([[
-      "#,##0.00", "@",
-      "#,##0.00", "@",
-      "0.00%", "@",
-      "#,##0.00", "@",
-    ]]);
-  fourKeysSheet.getRange(3, 2, 1, 8).copyTo(
-    fourKeysSheet.getRange(4, 2, 4, 8)
-  );
-  const today = new Date();
-  fourKeysSheet.getRange(3, 1, 5, 1).setValues(
-    [4,3,2,1,0].map((numberOfTwoWeek) => [Utilities.formatDate(new Date(new Date().setDate(today.getDate() - 14*numberOfTwoWeek)), "JST", "yyyy-MM-dd")])
-  );
-  fourKeysSheet.getRange(8, 1, 1, 1).setValues([
-    ["移行のデータの統計値を取得する場合はB~I列を上からペーストしA列は任意の値を入力てください。"]
-  ]);
-
-  const dfLtChart = fourKeysSheet.newChart()
-    .addRange(fourKeysSheet.getRange("A1:A1000"))
-    .addRange(fourKeysSheet.getRange("B1:B1000"))
-    .addRange(fourKeysSheet.getRange("D1:D1000"))
-    .setChartType(Charts.ChartType.COMBO)
-    .setNumHeaders(1)
-    .setOption("title", "デプロイ頻度と変更リードタイム")
-    .setOption("series",[
-      { targetAxisIndex: 0, legend: "デプロイ頻度(1日あたり)"},
-      { targetAxisIndex: 1, legend: "変更リードタイム"},
-    ])
-    .setOption("vAxes", [
-      {
-        title: "回数",
-        minValue: 0
-      },
-      {
-        title: "時間",
-        minValue: 0
-      },
-    ])
-    .setOption("hAxes", {
-      title: "Week"
-    })
-    .setPosition(1, 10, 0, 0)
-    .build();
-  fourKeysSheet.insertChart(dfLtChart);
-
-  const incidentChart = fourKeysSheet.newChart()
-    .addRange(fourKeysSheet.getRange("A1:A1000"))
-    .addRange(fourKeysSheet.getRange("F1:F1000"))
-    .addRange(fourKeysSheet.getRange("H1:H1000"))
-    .setChartType(Charts.ChartType.COMBO)
-    .setNumHeaders(1)
-    .setOption("title", "変更障害率と平均復旧時間")
-    .setOption("series",[
-      { targetAxisIndex: 0, legend: "変更障害率"},
-      { targetAxisIndex: 1, legend: "平均修復時間"},
-    ])
-    .setOption("vAxes", [
-      {
-        title: "％",
-        minValue: 0
-      },
-      {
-        title: "時間",
-        minValue: 0
-      },
-    ])
-    .setOption("hAxes", {
-      title: "Week"
-    })
-    .setPosition(20, 10, 0, 0)
-    .build();
-  fourKeysSheet.insertChart(incidentChart);
+  new SettingsSheet().initialize(pullRequestsSheetName);
+  new FourKeysSheet().initialize(pullRequestsSheetName);
+  
   ScriptApp.getProjectTriggers().filter(t => t.getHandlerFunction() === "getAllRepos").forEach(t => ScriptApp.deleteTrigger(t));
   ScriptApp.newTrigger("getAllRepos")
     .timeBased()
--- src/Sheets.ts
+++ src/Sheets.ts
@@ -1,8 +1,11 @@
-interface ISheet {
-  readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
-}
+// interface ISheet {
+//   readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
+//   readonly sheetName: string;
+// }
 
 abstract class Sheet {
+  abstract sheet: GoogleAppsScript.Spreadsheet.Sheet;
+  public static sheetName: string;
   public static readonly defaultSheet: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 
   protected getOrCreateSheet(sheetName: string): GoogleAppsScript.Spreadsheet.Sheet {
@@ -10,14 +13,13 @@ abstract class Sheet {
   }
 }
 
-export class SettingsSheet extends Sheet implements ISheet {
-  sheetName: string;
+export class SettingsSheet extends Sheet {
+  public static readonly sheetName: string = "分析設定";
   readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
 
   constructor() {
     super();
-    this.sheetName = "分析設定";
-    this.sheet = this.getOrCreateSheet(this.sheetName);
+    this.sheet = this.getOrCreateSheet(SettingsSheet.sheetName);
   }
 
   public initialize(pullRequestsSheetName: string): void {
@@ -53,4 +55,149 @@ export class SettingsSheet extends Sheet implements ISheet {
         ["平均修復時間のMeidum定条件(修復PRが初コミットからマージされるまで平均N時間以内ならMedium", "720"],
       ]);
   }
+}
+
+export class FourKeysSheet extends Sheet {
+  public static readonly sheetName: string = "FourKeys計測結果";
+  readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
+
+  constructor() {
+    super();
+    this.sheet = this.getOrCreateSheet(FourKeysSheet.sheetName);
+  }
+
+  public initialize(pullRequestsSheetName: string): void {
+    this.sheet.getRange(1, 1, 2, 9)
+      .setValues([
+        [
+          "集計日",
+          "デプロイ頻度", "",
+          "変更のリードタイム", "",
+          "変更失敗率", "",
+          "平均復旧時間", ""
+        ],
+        [
+          "",
+          "回数", "ランク",
+          "時間(hours)", "ランク",
+          "割合(%)", "ランク",
+          "時間(hours)", "ランク"
+        ]])
+      .setBackgroundRGB(
+        224, 224, 224
+      );
+    const formatRanges = [
+      this.sheet.getRange("C3:C1000"),
+      this.sheet.getRange("E3:E1000"),
+      this.sheet.getRange("G3:G1000"),
+      this.sheet.getRange("I3:I1000")
+    ];
+    const rules = this.sheet.getConditionalFormatRules();
+    rules.push(SpreadsheetApp.newConditionalFormatRule()
+      .whenTextEqualTo("Elite")
+      .setBackground("#b7e1cd")
+      .setRanges(formatRanges)
+      .build());
+    rules.push(SpreadsheetApp.newConditionalFormatRule()
+      .whenTextEqualTo("High")
+      .setBackground("#c9daf8")
+      .setRanges(formatRanges)
+      .build());
+    rules.push(SpreadsheetApp.newConditionalFormatRule()
+      .whenTextEqualTo("Low")
+      .setBackground("#f4cccc")
+      .setRanges(formatRanges)
+      .build());
+    rules.push(SpreadsheetApp.newConditionalFormatRule()
+      .whenTextEqualTo("Medium")
+      .setBackground("#fff2cc")
+      .setRanges(formatRanges)
+      .build());
+    this.sheet.setConditionalFormatRules(rules);
+
+    this.sheet.getRange(3, 2, 1, 8)
+      .setValues([[
+        `=SUM(MAP('${SettingsSheet.sheetName}'!B$2:B$1000, '${SettingsSheet.sheetName}'!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${SettingsSheet.sheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!I$2:I$100000000, FALSE, '${pullRequestsSheetName}'!A$2:A$100000000, a), 0))))/'${SettingsSheet.sheetName}'!E$2`,
+        `=IFS(B3>='${SettingsSheet.sheetName}'!E$5, "Elite", B3>='${SettingsSheet.sheetName}'!E$6, "High", B3>='${SettingsSheet.sheetName}'!E$7, "Medium", TRUE, "Low")`,
+        `=IF(B3 > 0, SUM(MAP('${SettingsSheet.sheetName}'!B$2:B$1000, '${SettingsSheet.sheetName}'!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), SUMIFS('${pullRequestsSheetName}'!G$2:G$100000000, '${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${SettingsSheet.sheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!I$2:I$100000000, FALSE, '${pullRequestsSheetName}'!A$2:A$100000000, a), 0)))) / (B3*'${SettingsSheet.sheetName}'!E$2), 0)`,
+        `=IFS(D3<='${SettingsSheet.sheetName}'!E$8, "Elite", D3<='${SettingsSheet.sheetName}'!E$9, "High", D3<='${SettingsSheet.sheetName}'!E$10, "Medium", TRUE, "Low")`,
+        `=IF(B3 > 0, SUM(MAP(${SettingsSheet.sheetName}!B$2:B$1000, ${SettingsSheet.sheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${SettingsSheet.sheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!I$2:I$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a), 0))))/(B3*'${SettingsSheet.sheetName}'!E$2), 0)`,
+        `=IFS(F3<='${SettingsSheet.sheetName}'!E$11, "Elite", F3<='${SettingsSheet.sheetName}'!E$12, "High", F3<='${SettingsSheet.sheetName}'!E$13, "Medium", TRUE, "Low")`,
+        `=IF(SUM(MAP(${SettingsSheet.sheetName}!B$2:B$1000, ${SettingsSheet.sheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${SettingsSheet.sheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!J$2:J$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a))))) > 0, SUM(MAP(${SettingsSheet.sheetName}!B$2:B$1000, ${SettingsSheet.sheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), SUMIFS('${pullRequestsSheetName}'!G$2:G$100000000, '${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${SettingsSheet.sheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!J$2:J$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a)))))/SUM(MAP(${SettingsSheet.sheetName}!B$2:B$1000, ${SettingsSheet.sheetName}!A$2:A$1000, LAMBDA(b, a, IF(OR(b<>FALSE, ISBLANK(b)), COUNTIFS('${pullRequestsSheetName}'!F$2:F$100000000, ">=" & A3-'${SettingsSheet.sheetName}'!E$2,'${pullRequestsSheetName}'!F$2:F$100000000, "<" & A3, '${pullRequestsSheetName}'!J$2:J$100000000, TRUE, '${pullRequestsSheetName}'!A$2:A$100000000, a))))), 0)`,
+        `=IFS(H3<='${SettingsSheet.sheetName}'!E$14, "Elite", H3<='${SettingsSheet.sheetName}'!E$15, "High", H3<='${SettingsSheet.sheetName}'!E$16, "Medium", TRUE, "Low")`
+      ]])
+      .setNumberFormats([[
+        "#,##0.00", "@",
+        "#,##0.00", "@",
+        "0.00%", "@",
+        "#,##0.00", "@",
+      ]]);
+    this.sheet.getRange(3, 2, 1, 8).copyTo(
+      this.sheet.getRange(4, 2, 4, 8)
+    );
+    const today = new Date();
+    this.sheet.getRange(3, 1, 5, 1).setValues(
+      [4,3,2,1,0].map((numberOfTwoWeek) => [Utilities.formatDate(new Date(new Date().setDate(today.getDate() - 14*numberOfTwoWeek)), "JST", "yyyy-MM-dd")])
+    );
+    this.sheet.getRange(8, 1, 1, 1).setValues([
+      ["移行のデータの統計値を取得する場合はB~I列を上からペーストしA列は任意の値を入力てください。"]
+    ]);
+
+    const dfLtChart = this.sheet.newChart()
+      .addRange(this.sheet.getRange("A1:A1000"))
+      .addRange(this.sheet.getRange("B1:B1000"))
+      .addRange(this.sheet.getRange("D1:D1000"))
+      .setChartType(Charts.ChartType.COMBO)
+      .setNumHeaders(1)
+      .setOption("title", "デプロイ頻度と変更リードタイム")
+      .setOption("series",[
+        { targetAxisIndex: 0, legend: "デプロイ頻度(1日あたり)"},
+        { targetAxisIndex: 1, legend: "変更リードタイム"},
+      ])
+      .setOption("vAxes", [
+        {
+          title: "回数",
+          minValue: 0
+        },
+        {
+          title: "時間",
+          minValue: 0
+        },
+      ])
+      .setOption("hAxes", {
+        title: "Week"
+      })
+      .setPosition(1, 10, 0, 0)
+      .build();
+    this.sheet.insertChart(dfLtChart);
+
+    const incidentChart = this.sheet.newChart()
+      .addRange(this.sheet.getRange("A1:A1000"))
+      .addRange(this.sheet.getRange("F1:F1000"))
+      .addRange(this.sheet.getRange("H1:H1000"))
+      .setChartType(Charts.ChartType.COMBO)
+      .setNumHeaders(1)
+      .setOption("title", "変更障害率と平均復旧時間")
+      .setOption("series",[
+        { targetAxisIndex: 0, legend: "変更障害率"},
+        { targetAxisIndex: 1, legend: "平均修復時間"},
+      ])
+      .setOption("vAxes", [
+        {
+          title: "％",
+          minValue: 0
+        },
+        {
+          title: "時間",
+          minValue: 0
+        },
+      ])
+      .setOption("hAxes", {
+        title: "Week"
+      })
+      .setPosition(20, 10, 0, 0)
+      .build();
+    this.sheet.insertChart(incidentChart);
+  }
+
 }
